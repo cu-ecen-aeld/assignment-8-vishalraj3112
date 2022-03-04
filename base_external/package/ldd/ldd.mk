8a739929ad3f5ec7a79df3c85c9262059338fdca
vishalraj3112 <vishal.raj15@siesgst.ac.in>
2022-03-04 14:51:36 -0700

    full-test passed

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-LDD_VERSION = 29ffbd2a359abb30da1dfb887162e178d5d05e4b #commit version number for A7-P2
+LDD_VERSION = ef640e4fe8bdb2e3c2f8c88723467e80673df03a #commit version number for A7-P2
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -15,8 +15,8 @@ LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 
 # Adding scull and misc-modules subdirectory.
-LDD_MODULE_SUBDIRS = scull
-LDD_MODULE_SUBDIRS += misc-modules
+LDD_MODULE_SUBDIRS = scull/
+LDD_MODULE_SUBDIRS += misc-modules/
 
 # TODO adding all the dependencies(scull and misc-modules).
 define LDD_INSTALL_TARGET_CMDS
--- base_external/rootfs_overlay/etc/init.d/S98lddmodules.sh
+++ base_external/rootfs_overlay/etc/init.d/S98lddmodules.sh
@@ -3,15 +3,15 @@
 case "$1" in 
 	start)
 		echo 'Starting scull and misc-modules'
-		modprobe hello
 		/usr/bin/module_load faulty
 		/usr/bin/scull_load
+		modprobe hello
 		;;
 	stop)
 		echo 'Stopping scull and misc-modules'
-		rmmod hello
 		/usr/bin/module_unload faulty
 		/usr/bin/scull_unload
+		rmmod hello
 		;;
 	*)
 	echo "Usage: $0 {start|stop} - to load and unload kernel modules"
